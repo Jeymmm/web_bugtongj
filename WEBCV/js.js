73e3a9b9c2d2402994d6a644c1ae6a9101de5cc1
Jeymmm <bugtongjamecris5@gmail.com>
2025-03-11 13:59:54 +0800

    Add files via upload

---
+++ WEBCV/js.js
@@ -0,0 +1,4 @@
+function toggleSocials() {
+    var socialsSection = document.getElementById("socials-details");
+    socialsSection.style.display = (socialsSection.style.display === "block") ? "none" : "block";
+}
